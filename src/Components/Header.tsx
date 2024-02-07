5c76bafc6f7cf227f9c4d9b86566b34a4c3cd12f
David Mikeladze <davidmikeladze00@gmail.com>
2024-02-07 16:02:48 +0400

    feat: add header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,14 @@
 function Header() {
   return (
-    <header>
-      <h1 className="text-[#D7E0FF]">pomodoro</h1>
+    <header className="flex flex-col items-center gap-[45px]">
+      <h1 className="text-[#D7E0FF] mt-[32px] text-[24px] font-bold">
+        pomodoro
+      </h1>
+      <div className="bg-[#161932] rounded-[50px] flex justify-evenly items-center w-[327px] h-[63px]">
+        <p className="text-[#D7E0FF] opacity-50">pomodoro</p>
+        <p className="text-[#D7E0FF] opacity-50">short break</p>
+        <p className="text-[#D7E0FF] opacity-50">long break</p>
+      </div>
     </header>
   );
 }
