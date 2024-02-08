3b49a1b0478a6e4b0391dbb4322e3ba477c5c5ca
David Mikeladze <davidmikeladze00@gmail.com>
2024-02-08 22:30:57 +0400

    feat: add header buttons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,40 @@
+import React, { useState } from "react";
+
 function Header() {
+  const [selectedOption, setSelectedOption] = useState("pomodoro");
+
+  const handleButtonClick = (option: React.SetStateAction<string>) => {
+    setSelectedOption(option);
+  };
+
   return (
     <header className="flex flex-col items-center gap-[45px]">
       <h1 className="text-[#D7E0FF] mt-[32px] text-[24px] font-bold">
         pomodoro
       </h1>
-      <div className="bg-[#161932] rounded-[50px] flex justify-evenly items-center w-[327px] h-[63px]">
-        <p className="text-[#D7E0FF] opacity-50">pomodoro</p>
-        <p className="text-[#D7E0FF] opacity-50">short break</p>
-        <p className="text-[#D7E0FF] opacity-50">long break</p>
+
+      <div className="bg-[#161932] rounded-[50px] flex justify-between items-center w-[327px] h-[63px]">
+        <button
+          className="bg-[#4D5FE2] rounded-[50px] px-4 py-2 text-white"
+          onClick={() => handleButtonClick("pomodoro")}
+        >
+          Pomodoro
+        </button>
+        <button
+          className="bg-[#4D5FE2] rounded-[50px] px-4 py-2 text-white"
+          onClick={() => handleButtonClick("short break")}
+        >
+          Short Break
+        </button>
+        <button
+          className="bg-[#4D5FE2] rounded-[50px] px-4 py-2 text-white"
+          onClick={() => handleButtonClick("long break")}
+        >
+          Long Break
+        </button>
+        <p className="text-[#D7E0FF] opacity-50">{selectedOption}</p>
       </div>
+      <div className={`w-[105.2px] h-[48px] rounded-[26.5px]`}></div>
     </header>
   );
 }
