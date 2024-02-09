c2b92749d17ddeaf730d6f5536c06bd6625fb8b2
David Mikeladze <davidmikeladze00@gmail.com>
2024-02-09 23:39:19 +0400

    feat: add react circular progress bar

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import Pomodoro from "./Pomodoro";
 
 function Layout() {
   return (
     <div>
       <Header />
+      <Pomodoro />
       <Outlet />
     </div>
   );
